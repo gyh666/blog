50fef8e1cfc9adc142abdb7cf54f9c869fd18ccb
gyh666 <1303678354@qq.com>
2020-07-06 09:42:58 +0800

    项目整理

--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -4,13 +4,8 @@ var router = express.Router();
 const { login } = require('../controller/user');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
 
-// 登录验证
-const loginCheck = (req, res, next) => {
-  next();
-}
-
 /* GET users listing. */
-router.post('/login', /* loginCheck, */ async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
   const { username, password } = req.body;
   console.log(username, password);
   let result = await login(username, password);
@@ -22,14 +17,5 @@ router.post('/login', /* loginCheck, */ async (req, res, next) => {
     return res.json(new ErrorModel('登录失败'));
   }
 });
-
-/* test. */
-// router.get('/login-test', (req, res, next) => {
-//   const flag = req.session.username;
-//   return res.json({
-//     code: flag ? 200 : 0,
-//     msg: flag ? '登录成功' : '登录失败'
-//   });
-// });
 
 module.exports = router;
